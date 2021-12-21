aff1854a150f43e943f08f081c5f773e17a9a18d
Larry Chen <larrybrown901120@gmail.com>
2021-12-21 16:05:29 +0800

    Add the recursive function with some errors

--- DataStructure/Homework/HW3/F74092269_hw3-1.cpp
+++ DataStructure/Homework/HW3/F74092269_hw3-1.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 
 using namespace std;
 
 class TreeNode{
+
+    public:
+        TreeNode(int value, TreeNode* lft, TreeNode* rgt){
+            this->value = value;
+            this->leftChild = lft;
+            this->rightChild = rgt;
+        }
+
     private:
         int value;
         TreeNode* leftChild;
         TreeNode* rightChild;
+
     friend class BinaryTree;
 };
 
 class BinaryTree{
+
     public:
-        void add(int);
+        void add(int, TreeNode*);
         void preorder(TreeNode*);
         void postorder(TreeNode*);
+        void preToIn(int*, int*, int, TreeNode*);
+        void postToIn(int*, int*, int, TreeNode*);
+        TreeNode* getRoot(){
+            return this->root;
+        }
     private:
-        TreeNode* root;
-
+        TreeNode* root = NULL;
 };
 
-void preToIn(int*, int*, int);
-void postToIn(int*, int*, int);
 
 int main() {
     int m;
@@ -32,6 +43,7 @@ int main() {
     for (int i = 0; i < m; i++){
         int n;
         string type;
+        BinaryTree bt;
 
         cin.ignore();
         getline(cin, type);
@@ -44,23 +56,59 @@ int main() {
             cin >> b[i];
 
         if (type.compare("preorder-inorder") == 0){
-
+            bt.preToIn(a, b, n, bt.getRoot());
+            bt.postorder(bt.getRoot());
         }else if (type.compare("postorder-inorder") == 0){
-
+            bt.postToIn(a, b, n, bt.getRoot());
+            bt.preorder(bt.getRoot());
         }else{
             cout << "ERROR" << endl;
         }
     }
 
-
-
-
     return 0;
 }
 
-void preToIn(int* pre, int* in, int size){
-    int visit = *pre;
+void BinaryTree::add(int value, TreeNode* addedNode){
+    addedNode = new TreeNode(value, NULL, NULL);
+}
+
+void BinaryTree::preToIn(int* pre, int* in, int size, TreeNode* curr){
+
+    if (size == 0){
+        cout << "Zero\n";
+        return;
+    }
+
+    int visit = pre[0];
     int position;
+    this->add(visit, curr);
+
+    if (size == 1){
+        cout << "One\n";
+        return;
+    }
+
+    for (int i = 0; i < size; i++){
+        if (in[i] == visit){
+            position = i;
+            break;
+        }
+    }
+
+    preToIn(&pre[1], in, position, curr->leftChild);
+    preToIn(&pre[1 + position], &in[1 + position], size - position - 1, curr->rightChild);
+}
+
+void BinaryTree::postToIn(int* post, int* in, int size, TreeNode* curr){
+
+    int visit = *(post + size - 1);
+    int position;
+    this->add(visit, curr);
+
+    if (size == 1)
+        return;
+
 
     for (int i = 0; i < size; i++){
         if (*(in + i) == visit){
@@ -69,6 +117,23 @@ void preToIn(int* pre, int* in, int size){
         }
     }
 
-    preToIn(pre + 1, in, position);
-    preToIn(pre + 1 + position, in + position + 1, size - position - 1);
+    postToIn(post, in, position, curr->leftChild);
+    postToIn(post + position, in + position + 1, size - position - 1, curr->rightChild);
+}
+
+void BinaryTree::preorder(TreeNode* curr){
+    if (!curr)
+        return;
+
+    cout << curr->value << " ";
+    this->preorder(curr->leftChild);
+    this->preorder(curr->rightChild);
+}
+void BinaryTree::postorder(TreeNode* curr){
+    if (!curr)
+        return;
+
+    this->postorder(curr->leftChild);
+    this->postorder(curr->rightChild);
+    cout << curr->value << " ";
 }
